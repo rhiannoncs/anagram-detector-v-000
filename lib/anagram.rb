a68c1d58bdc8e1f7bdfb6b03ea87577215160f29
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-30 04:09:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,4 +4,14 @@ class Anagram
   def initialize(word)
     @word = word
   end
+  
+  def match(array)
+    matches = []
+    split_word = @word.split("").sort
+    array.each do |array_word|
+      split_array_word = array_word.split("").sort
+      matches << array_word if split_word == split_array_word
+    end
+    matches
+  end
 end
